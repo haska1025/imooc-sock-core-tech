bcaa94ede14ac6dd6999a2528a0876946fd55e76
haska1025 <280978693@qq.com>
2020-01-05 17:30:10 +0800

    Add getaddrinfo

--- 03-15_通过域名进行编程/domain_name_resolve.c
+++ 03-15_通过域名进行编程/domain_name_resolve.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 void gethostbyname_case(const char *hostname)
 {
@@ -32,10 +33,79 @@ void gethostbyname_case(const char *hostname)
     }
 }
 
+void gethostbyaddr_case(const char *ip)
+{
+    struct hostent *host = NULL;
+    struct in_addr addr;
+
+    inet_aton(ip, &addr);
+
+    host = gethostbyaddr(&addr, sizeof(addr), AF_INET);
+    if (!host){
+        printf("gethostbyaddr(%s) failed! errno(%d) errorstr(%s)\n",
+                ip, errno, hstrerror(errno));
+        return;
+    }
+
+    printf("ip(%s) domainname(%s)\n", ip, host->h_name);
+}
+
+void getaddrinfo_case(const char *hostname, const char *service)
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int s;
+
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = 0;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    s = getaddrinfo(hostname, service, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        return;
+    }
+
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        char buf[128] = {0};
+        if (rp->ai_family == AF_INET){
+            inet_ntop(rp->ai_family, &((struct sockaddr_in*)rp->ai_addr)->sin_addr, buf, 127);
+        }else if (rp->ai_family == AF_INET6){
+            inet_ntop(rp->ai_family, &((struct sockaddr_in6*)rp->ai_addr)->sin6_addr, buf, 127);
+        }
+
+        printf("hostname(%s) service(%s) family(%d) socktype(%d) protocol(%d) addr(%s)\n",
+                hostname,
+                service,
+                rp->ai_family,
+                rp->ai_socktype,
+                rp->ai_protocol,
+                buf);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+}
 
 int main(int argc, char *argv[])
 {
+    /*
     gethostbyname_case("www.baidu.com");
+    gethostbyname_case("www.google.com");
+
+    gethostbyaddr_case("114.114.114.114");
+    gethostbyaddr_case("4.4.4.4");
+    gethostbyaddr_case("8.8.8.8");
+*/
+    getaddrinfo_case("www.imooc.com", NULL);
+
+ //   getaddrinfo_case(NULL, "80");
 
     return 0;
 }
